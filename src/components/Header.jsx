054e8c595e7f51d58bec37c2e4f6112da6d667e0
MajorBoston <lutsykoleksandr1990@gmail.com>
2021-04-27 13:28:59 +0300

    Source code

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='https://lh3.googleusercontent.com/proxy/zsVrZfkee2eZ-oQzl4RRsMfbueVXMei5Vfw-9BlKSlSSb4AXTls3aHdk42cpFH5i0YBlWZi-wNqAzcCmQyoI4kO0_WSlV8jr8rnRIbIRcW9eLdPCfeHSfNyE8U5p4A' alt="" ></img>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img className='picDC' src='https://vcomicse.ru/wp-content/uploads/2017/10/155811_P4Y5F5sLoJ_rebirth_splash_log-1.jpg' alt=''></img>
+      </div>
+      <div>
+        ava + description
+    </div>
+      <div>
+        My posts
+      <div>
+          New post
+      </div>
+        <div>
+          <div>
+            post 1
+        </div>
+          <div>
+            post 1
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
